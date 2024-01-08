17144e606cb6e7b069f2b0002ce965f2cfa85b39
FrancescoCesari97 <freedus97@gmail.com>
2024-01-08 15:27:50 +0100

    Aggiunti i prompt delle domande

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+//Chiedi all'utente il suo nome
+
+const Name = prompt('Come ti chiami?')
+console.log(Name)
+
+//Chiedi all'utente il suo cognome
+
+const Surname = prompt('Qual è il tuo cognome?')
+console.log(Surname)
+
+//Chiedi il suo colore preferito
+
+const Color = prompt('Qual è il tuo colore preferito')
+console.log(Color)
