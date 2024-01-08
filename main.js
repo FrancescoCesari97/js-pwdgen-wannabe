7235a1b3e57413337f74a4cebb55e75de8b2afb5
FrancescoCesari97 <freedus97@gmail.com>
2024-01-08 16:59:59 +0100

    stampa risultato

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 
 
-//Chiedi all'utente il suo nome
+
+
+const resultElement = document.getElementById('main-title');
+
+
+// Chiedi all'utente il suo nome
 
 const Name = prompt('Come ti chiami?')
 console.log(Name)
 
-//Chiedi all'utente il suo cognome
+// Chiedi all'utente il suo cognome
 
 const Surname = prompt('Qual è il tuo cognome?')
 console.log(Surname)
 
-//Chiedi il suo colore preferito
+// Chiedi il suo colore preferito
 
 const Color = prompt('Qual è il tuo colore preferito')
 console.log(Color)
+
+// scrivere sulla pagina nomecognomecolorepreferito23
+
+resultElement.innerHTML = `Il mio nome Cognome e colore preferito è <b>${Name + Surname + Color + 23} <b/> `;
+
